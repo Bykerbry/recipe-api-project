7e9a5d70fd943290ef9cb8ebf3364f449a9122d3
Bryce <bykerbry@umich.edu>
2020-02-05 20:10:06 -0500

    good luck fixing the routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FavoritesPageComponent } from './favorites-page/favorites-page.component';
-import { RecipeListComponent } from './recipe-list/recipe-list.component'
 import { SearchCriteriaComponent } from './search-criteria/search-criteria.component';
 
 
 const routes: Routes = [
   { path: 'Favorites', component: FavoritesPageComponent },
-  { path: 'Home', component: RecipeListComponent},
-  { path: 'Random', component: SearchCriteriaComponent}
+  { path: 'Home', component: SearchCriteriaComponent}
 ];
 
 @NgModule({
