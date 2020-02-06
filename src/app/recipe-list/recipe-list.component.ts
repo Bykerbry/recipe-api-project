f793b3b9c75fd6b7ed2c60a8d06e24cea6add1b1
brielle <briellemarie28@gmail.com>
2020-02-05 22:01:37 -0500

    started faves

--- src/app/recipe-list/recipe-list.component.ts
+++ src/app/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { RecipeApiService } from '../recipe-api.service';
-import { IParams } from '../search-params.interface'
+import { IParams } from '../search-params.interface';
+
 
 @Component({
   selector: 'app-recipe-list',
@@ -9,7 +10,10 @@ import { IParams } from '../search-params.interface'
 })
 export class RecipeListComponent implements OnInit {
   recipes: any[];
+  favorites: any [] = []
+
   @Input() searchParameters: IParams;
+  
   constructor(private _service: RecipeApiService) { }
 
   ngOnInit() { 
@@ -17,9 +21,21 @@ export class RecipeListComponent implements OnInit {
       this.recipes = data.hits.map((index: any) => index.recipe);
     })
   }
-
   getList() {
     console.log(this.searchParameters);
     console.log(this.recipes);
   }
-}
+  @Output () faves = new EventEmitter <boolean>();
+  
+  isFave : true;
+  addToFavorites() {
+    this.faves.emit();
+  }
+  onFavorite(recipe: any) {
+      this.favorites.push(recipe);
+
+    }
+  }
+  
+
+  
